7c735a989321a36231878a65bbf0d1f4f7985560
Andrey Golubev <laissezfaireisfair@gmail.com>
2020-09-06 14:39:39 +0700

    Update first test to verbose steps

--- 1_CycleContainer/test/1_Making.cpp
+++ 1_CycleContainer/test/1_Making.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include "CycleContainer.hpp"
 
+using namespace std;
 using namespace CycleContainer;
 
 int main() {
-  Container<int> emptyCont       = Container<int>();
-  Container<int> allocatedCont   = Container<int>(5);
+  cout                 << "Making empty container..........";
+  Container<int> emptyCont = Container<int>();
+  cout << "OK" << endl << "Making allocated container......";
+  Container<int> allocatedCont = Container<int>(5);
+  cout << "OK" << endl << "Making initialised container....";
   Container<int> initialisedCont = Container<int>(42, 5);
-  Container<int> copyCont        = Container<int>(initialisedCont);
+  cout << "OK" << endl << "Making copy of container........";
+  Container<int> copyCont = Container<int>(initialisedCont);
+  cout << "OK" << endl << "Copying to existing container...";
   emptyCont = initialisedCont;
+  cout << "OK" << endl << "All right. Exiting." << endl;
   return 0;
 }
