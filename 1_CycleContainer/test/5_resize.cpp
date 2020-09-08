4928dc023108d5ca8f58b918412bbbd11a44b664
Andrey Golubev <laissezfaireisfair@gmail.com>
2020-09-08 09:52:25 +0700

    Add resize test

---
+++ 1_CycleContainer/test/5_resize.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include "CycleContainer.hpp"
+
+using namespace std;
+using namespace CycleContainer;
+
+bool check_buffer(Container<int> & buffer, vector<int> const & expected) {
+  cout << "Checking buffer...";
+  if (buffer.get_size() != expected.size()) {
+    cout << "FAILED" << endl << "Abort." << endl;
+    return false;
+  }
+  for (uint64_t i = 0; i < expected.size(); ++i) {
+    if (buffer[i] != expected[i]) {
+      cout << "FAILED" << endl << "Abort." << endl;
+      return false;
+    }
+  }
+  cout << "OK" << endl;
+  return true;
+}
+
+bool do_resize(Container<int> & buffer, uint64_t const newCapacity) {
+  cout << "Resizing..........";
+  try {
+    buffer.resize(newCapacity);
+    cout << "OK" << endl;
+  }
+  catch(exception & e) {
+    cout << "FAILED" << endl << e.what() << endl << "Abort." << endl;
+    return false;
+  }
+  return true;
+}
+
+bool check_capacity(Container<int> const & buffer, unsigned int const expected) {
+  cout << "Checking capacity.";
+  if (buffer.get_capacity() == expected) {
+    cout << "OK" << endl;
+  } else {
+    cout << "FAILED" << endl << "Abort." << endl;
+    return false;
+  }
+  return true;
+}
+
+int main() {
+  cout << "Making buffer.....";
+  Container<int> buffer = Container<int>(5, 42);
+  cout << "OK" << endl;
+
+  cout << "---Increase check---"  << endl;
+  if (!do_resize(buffer, 6))
+    return 1;
+  if (!check_capacity(buffer, 6))
+    return 2;
+  if (!check_buffer(buffer, vector<int>(5, 42)))
+    return 3;
+
+  cout << "---Decrease check---"  << endl;
+  if (!do_resize(buffer, 5))
+    return 4;
+  if (!check_capacity(buffer, 5))
+    return 5;
+  if (!check_buffer(buffer, vector<int>(5, 42)))
+    return 6;
+
+  cout << "All right. Exiting." << endl;
+  return 0;
+}
