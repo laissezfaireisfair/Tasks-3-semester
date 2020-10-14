0e9ae6d767e2424cf041c9e674aecd63f17d0c72
Andrey Golubev <laissezfaireisfair@gmail.com>
2020-10-14 13:10:19 +0700

    Finish comparsion log

--- 2_SortLog/include/Elem.h
+++ 2_SortLog/include/Elem.h
@@ -7,10 +7,9 @@ public:
 	Elem(int const value, unsigned int const position);
 	bool operator<(Elem & other);
 	Elem & operator=(Elem const & other);
-	int get_value() const;
 	unsigned int get_position() const;
-	void set_position(unsigned int const newPosition);
 private:
 	int m_value;
 	unsigned int m_position;
+    bool m_contained;
 };
--- 2_SortLog/src/Elem.cpp
+++ 2_SortLog/src/Elem.cpp
@@ -3,16 +3,19 @@
 Elem::Elem() {
 	m_value = -1;
 	m_position = 0;
+	m_contained = false;
 }
 
 Elem::Elem(int const value, unsigned int const position) {
 	m_value = value;
 	m_position = position;
+	m_contained = true;
 }
 
 bool Elem::operator<(Elem & other) {
-	ConsoleLogger::print_comparsion(*this, other);
 	if (m_value < other.m_value) { // Comparsion succecful => it will be swapped (I hope)
+		if (m_contained && other.m_contained)
+			ConsoleLogger::print_comparsion(*this, other);
 		unsigned int tempPos = m_position;
 		m_position = other.m_position;
 		other.m_position = tempPos;
@@ -26,17 +29,10 @@ Elem & Elem::operator=(Elem const & other) {
 		return *this;
 	m_value = other.m_value;
 	m_position = other.m_position;
+	m_contained = other.m_contained;
 	return *this;
-}
-
-int Elem::get_value() const {
-	return m_value;
 }
 
 unsigned int Elem::get_position() const {
 	return m_position;
-}
-
-void Elem::set_position(unsigned int const newPosition) {
-	m_position = newPosition;
 }
