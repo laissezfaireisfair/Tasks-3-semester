7a8747e93f3989c5e2faee147a3141d169f134e0
Andrey Golubev <laissezfaireisfair@gmail.com>
2020-10-16 18:42:13 +0700

    Update makefile and tabs

--- 2_SortLog/src/Elem.cpp
+++ 2_SortLog/src/Elem.cpp
@@ -4,17 +4,17 @@
 #include "Assignment.h"
 
 Elem::Elem() {
-	m_value = -1;
-	m_position = 0;
-	m_contained = false;
-	m_logger = nullptr;
+    m_value = -1;
+    m_position = 0;
+    m_contained = false;
+    m_logger = nullptr;
 }
 
 Elem::Elem(int const value, unsigned int const position, ConsoleLogger & logger) {
-	m_value = value;
-	m_position = position;
-	m_contained = true;
-	m_logger = &logger;
+    m_value = value;
+    m_position = position;
+    m_contained = true;
+    m_logger = &logger;
 }
 
 Elem::Elem(Elem const & other) {
@@ -25,24 +25,24 @@ Elem::Elem(Elem const & other) {
 }
 
 bool Elem::operator<(Elem & other) {
-	if (m_value < other.m_value) {
-	    if (m_contained && other.m_contained)
-	        m_logger->operation_happened(Comparison(position(), other.position()));
-		return true;
-	}
-	return false;
+    if (m_value < other.m_value) {
+        if (m_contained && other.m_contained)
+            m_logger->operation_happened(Comparison(position(), other.position()));
+        return true;
+    }
+    return false;
 }
 
 Elem & Elem::operator=(Elem const & other) {
-	if (&other == this)
-		return *this;
-	if (m_contained && other.m_contained)
-	    m_logger->operation_happened(Assignment(position(), other.position()));
-	m_value = other.m_value;
-	m_position = other.m_position;
-	m_contained = other.m_contained;
-	m_logger = other.m_logger;
-	return *this;
+    if (&other == this)
+        return *this;
+    if (m_contained && other.m_contained)
+        m_logger->operation_happened(Assignment(position(), other.position()));
+    m_value = other.m_value;
+    m_position = other.m_position;
+    m_contained = other.m_contained;
+    m_logger = other.m_logger;
+    return *this;
 }
 
 bool Elem::operator==(Elem const & other) const {
@@ -54,9 +54,9 @@ bool Elem::operator!=(Elem const & other) const {
 }
 
 unsigned int Elem::position() const {
-	return m_position;
+    return m_position;
 }
 
 int Elem::value() const {
-	return m_value;
+    return m_value;
 }
